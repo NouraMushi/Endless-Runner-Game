bafc80f4c377b4224b4d5879031db8a41bf8d54d
Noura Abdallah Mushi <119770040+NouraMushi@users.noreply.github.com>
2024-12-18 11:25:28 +0300

    UI & Particle Effects

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        Time.timeScale = 1;
+        transform.DOMoveY(-230, 1f);
+    }
+
+    public void PauseGame()
+    {
+        transform.DOMoveY(500, 1f).OnComplete(() =>
+        {
+            Time.timeScale = 0;
+        });
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
